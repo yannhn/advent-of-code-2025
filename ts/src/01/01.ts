2f0e351827fa6b682d0e548126201f071ca9bfbb
yannhn <yanso1@posteo.de>
2025-12-06 01:27:43 +0100

    01 - 02

--- ts/src/01/01.ts
+++ ts/src/01/01.ts
@@ -1,7 +1,11 @@
-const file = Bun.file("./example.txt");
+// NOTE: 01 - Part 1
+
+const file = Bun.file("./input.txt");
 const content = await file.text();
 
 const lines: string[] = content.trim().split("\n");
+
+console.log(lines);
 
 function calcPassword(startValue: number, input: string[]) {
   let zeroCounter = 0;
@@ -26,4 +30,37 @@ function calcPassword(startValue: number, input: string[]) {
   return zeroCounter;
 }
 
-console.log(calcPassword(50, lines));
+console.log("START:", 50);
+
+console.log("pwCounter", calcPassword(50, lines));
+
+// NOTE: 01 - Part 2
+
+function calcDialPassword(startValue: number, input: string[]) {
+  let zeroCounter = 0;
+  const limit = 100;
+
+  for (const item of input) {
+    const cleanItem = item.trim();
+    const direction = item[0];
+    let value = Number(cleanItem.slice(1));
+
+    for (let i = 0; i < value; i++) {
+      if (direction === "R") {
+        startValue = (startValue + 1) % limit;
+      } else if (direction === "L") {
+        startValue = (((startValue - 1) % limit) + limit) % limit;
+      }
+
+      if (startValue === 0) {
+        zeroCounter++;
+      }
+    }
+  }
+
+  return zeroCounter;
+}
+
+console.log("START:", 50);
+
+console.log("pwCounter", calcDialPassword(50, lines));
